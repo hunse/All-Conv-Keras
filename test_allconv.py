Fixed test_allconv scaling and added quantize

test_allconv.py
@@ -1,43 +1,51 @@
 from __future__ import print_function
 
-import sys
+import argparse
 
-import tensorflow as tf
-from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dropout, Activation, Convolution2D, GlobalAveragePooling2D, merge
-from keras.utils import np_utils
-from keras.optimizers import SGD
+import keras
 from keras import backend as K
-from keras.models import Model, load_model
-from keras.layers.core import Lambda
-from keras.callbacks import ModelCheckpoint
-import pandas
-import cv2
 import numpy as np
 
-modelfile = sys.argv[1] if len(sys.argv) > 1 else 'weights.hdf5'
+
+def preprocess(x):
+    x = x.astype('float32')
+    x /= 255
+    x -= 0.5
+    return x
+
+
+def quantize(x, bits):
+    levels = 2**bits
+    max_x = np.abs(x).max()
+    alpha = 0.5 * levels / max_x
+    y = np.round(alpha * x) / alpha
+    return y
+
+
+parser = argparse.ArgumentParser(description="Test allconv CIFAR-10 network")
+parser.add_argument('--quantize', action='store_true')
+parser.add_argument('modelfile', nargs='?', default='weights.hdf5', help="Model file to load")
+args = parser.parse_args()
 
 K.set_image_dim_ordering('tf')
 
-
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-#print('X_train shape:', X_train.shape)
-#print(X_train.shape[0], 'train samples')
-#print(X_test.shape[0], 'test samples')
-#print (X_train.shape[1:])
+(X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
+X_train, X_test = preprocess(X_train), preprocess(X_test)
 
 nb_classes = 10
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+Y_train = keras.utils.np_utils.to_categorical(y_train, nb_classes)
+Y_test = keras.utils.np_utils.to_categorical(y_test, nb_classes)
 
-model = load_model(modelfile)
+model = keras.models.load_model(args.modelfile)
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+if args.quantize:
+    for layer in model.layers:
+        if isinstance(layer, keras.layers.Conv2D):
+            w, b = layer.get_weights()
+            print(w[:, :, 0, 0])
+            w = quantize(w, 8)
+            print(w[:, :, 0, 0])
+            layer.set_weights((w, b))
 
 e = model.evaluate(X_test, Y_test, batch_size=32)
 print("Loss: %0.3e, Accuarcy: %0.3f" % (e[0], e[1]))